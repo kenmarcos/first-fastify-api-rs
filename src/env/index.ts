0994a3bc751b5d8c1799820caf5b1f9b0abc23e9
Marcos Kenji Kuribayashi <marcosken13@gmail.com>
2024-04-30 16:42:58 -0300

    feat: add environment variable management

---
+++ src/env/index.ts
@@ -0,0 +1,22 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+// NODE_ENV: diz em qual ambiente a aplicação está rodando.
+// O valor é informado automaticamente pelas ferramentas que estão sendo
+// utilizadas para executar a aplicação
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
+  DATABASE_URL: z.string(), // é um valor obrigatório
+  PORT: z.number().default(3333),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success === false) {
+  console.error('❌ Invalid environment variables', _env.error.format())
+
+  throw new Error('Invalid environment variables.')
+}
+
+export const env = _env.data
